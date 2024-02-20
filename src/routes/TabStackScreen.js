9495a301c43d913109a611668e7c0915debf99a5
AliAksoyy <alifeyza4017@gmail.com>
2024-02-20 17:05:57 +0300

    added

--- src/routes/TabStackScreen.js
+++ src/routes/TabStackScreen.js
@@ -1,8 +1,10 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, TouchableOpacity} from 'react-native';
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import ContactScreen from '../screens/ContactScreen';
 import ChatsScreen from '../screens/ChatsScreen';
+import Feather from 'react-native-vector-icons/Feather';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,12 +14,26 @@ export default function TabStackScreen() {
       <Tab.Screen
         name="Contacts"
         component={ContactScreen}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, focused, size}) => (
+            <TouchableOpacity>
+              <Feather name="users" size={25} />
+            </TouchableOpacity>
+          ),
+        }}
       />
       <Tab.Screen
         name="Chats"
         component={ChatsScreen}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, focused, size}) => (
+            <TouchableOpacity>
+              <Ionicons name="chatbubble-outline" size={25} />
+            </TouchableOpacity>
+          ),
+        }}
       />
     </Tab.Navigator>
   );
