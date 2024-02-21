95c1ea1c85554bc4a34a77f61aa9091fb7a0e40f
AliAksoyy <alifeyza4017@gmail.com>
2024-02-21 11:19:06 +0300

    added

--- src/components/MessageContent.tsx
+++ src/components/MessageContent.tsx
@@ -7,27 +7,39 @@ export default function MessageContent() {
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, neque.Eaque expedita ab harum deserunt',
       'Aliquid exercitationem officialaborum impedit Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
       'A, neque.Eaque expedita ab harum deserunt! Aliquid exercitationem officialaborum impedit ',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, neque.Eaque expedita ab harum deserunt',
+      'Aliquid exercitationem officialaborum impedit Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+      'A, neque.Eaque expedita ab harum deserunt! Aliquid exercitationem officialaborum impedit ',
     ],
   });
 
-  const windowWidth = Math.floor(Dimensions.get('window').width) / 2;
+  const windowWidth = (Math.floor(Dimensions.get('window').width) * 2) / 3;
 
- const styles = StyleSheet.create({
-   container: {flex: 1, margin: 25},
-   messageContainer: {
-     backgroundColor: '#e2dfdf',
-     marginVertical: 10,
-     padding: 20,
-     borderRadius: 25,
-     width: windowWidth,
-   },
-   message: {
-     textAlign: 'left',
-     fontSize: 15,
-     color: '#262626',
-     fontWeight: '600',
-   },
- });
+  const styles = StyleSheet.create({
+    container: {flex: 1, margin: 25},
+
+    messageContainer: {
+      marginVertical: 10,
+      padding: 15,
+      borderRadius: 25,
+      width: windowWidth,
+    },
+    messageContainerLeft: {alignSelf: 'flex-start', backgroundColor: '#e2dfdf'},
+    messageContainerRight: {
+      alignSelf: 'flex-end',
+      backgroundColor: '#FF9134',
+    },
+    message: {
+      textAlign: 'left',
+      fontSize: 15,
+
+      fontWeight: '500',
+    },
+    messageRight: {
+      color: '#fff',
+    },
+    messageLeft: {color: '#3a3737'},
+  });
 
   return (
     <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
@@ -35,13 +47,26 @@ export default function MessageContent() {
         .flat()
         .map((message, index) => {
           return (
-            <View style={styles.messageContainer} key={index}>
-              <Text style={styles.message}>{message}</Text>
+            <View>
+              <View
+                style={[
+                  index % 2 === 0
+                    ? styles.messageContainerLeft
+                    : styles.messageContainerRight,
+                  styles.messageContainer,
+                ]}
+                key={index}>
+                <Text
+                  style={[
+                    index % 2 === 0 ? styles.messageLeft : styles.messageRight,
+                    styles.message,
+                  ]}>
+                  {message}
+                </Text>
+              </View>
             </View>
           );
         })}
     </ScrollView>
   );
 }
-
-
