03e453897d0ce1c8f0a95c7721c938810ff35242
AliAksoyy <alifeyza4017@gmail.com>
2024-02-21 10:34:34 +0300

    added

--- src/components/MessageContent.tsx
+++ src/components/MessageContent.tsx
@@ -1,55 +1,47 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {ScrollView, StyleSheet, Text, View, Dimensions} from 'react-native';
+import React, {useState} from 'react';
 
 export default function MessageContent() {
+  const [message, setMessage] = useState({
+    message: [
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, neque.Eaque expedita ab harum deserunt',
+      'Aliquid exercitationem officialaborum impedit Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+      'A, neque.Eaque expedita ab harum deserunt! Aliquid exercitationem officialaborum impedit ',
+    ],
+  });
+
+  const windowWidth = Math.floor(Dimensions.get('window').width) / 2;
+
+ const styles = StyleSheet.create({
+   container: {flex: 1, margin: 25},
+   messageContainer: {
+     backgroundColor: '#e2dfdf',
+     marginVertical: 10,
+     padding: 20,
+     borderRadius: 25,
+     width: windowWidth,
+   },
+   message: {
+     textAlign: 'left',
+     fontSize: 15,
+     color: '#262626',
+     fontWeight: '600',
+   },
+ });
+
   return (
     <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
-      <Text>MessageContent</Text>
+      {Object.values(message)
+        .flat()
+        .map((message, index) => {
+          return (
+            <View style={styles.messageContainer} key={index}>
+              <Text style={styles.message}>{message}</Text>
+            </View>
+          );
+        })}
     </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {flex: 1, margin: 25},
-});
+
