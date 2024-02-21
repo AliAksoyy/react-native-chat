e0f9ba8b86fd7d43ef89a72fde24dcb24ebb4bde
AliAksoyy <alifeyza4017@gmail.com>
2024-02-21 11:54:09 +0300

    added

--- src/components/MessageTyping.tsx
+++ src/components/MessageTyping.tsx
@@ -1,14 +1,38 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 export default function MessageTyping() {
   return (
     <View style={styles.container}>
-      <Text>MessageTyping</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Type something"
+        placeholderTextColor="#000"
+      />
+      <TouchableOpacity style={styles.btn}>
+        <AntDesign name="arrowup" color="#FF9134" size={26} />
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {height: 80, paddingHorizontal: 25, backgroundColor: '#FAFAFA'},
+  container: {
+    height: 80,
+    paddingHorizontal: 25,
+    backgroundColor: '#FFF',
+    flexDirection: 'row',
+    alignItems: 'center',
+
+    justifyContent: 'space-between',
+  },
+  input: {flex: 1, fontSize: 18, color: '262626'},
+  btn: {padding: 7, backgroundColor: '#fafafa'},
 });
