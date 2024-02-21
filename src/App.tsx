06910b337331d1cc393ec22f01e8cd0e63371ca3
AliAksoyy <alifeyza4017@gmail.com>
2024-02-21 14:19:13 +0300

    added

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {LoginProvider} from './context/LoginContext';
 import AuthStackScreen from './routes/AuthStackScreen';
+import {store} from './app/store';
+import {Provider} from 'react-redux';
 
 export default function App() {
   return (
-    <LoginProvider>
-      <NavigationContainer>
-        <AuthStackScreen />
-      </NavigationContainer>
-    </LoginProvider>
+    <Provider store={store}>
+      <LoginProvider>
+        <NavigationContainer>
+          <AuthStackScreen />
+        </NavigationContainer>
+      </LoginProvider>
+    </Provider>
   );
 }
--- src/routes/AuthStackScreen.js
+++ src/routes/AuthStackScreen.js
@@ -5,6 +5,7 @@ import SignInScreen from '../screens/SignInScreen';
 import MessageDetails from '../screens/MessageDetails';
 import {useLoginContext} from '../context/LoginContext';
 import TabStackScreen from './TabStackScreen';
+import {useSelector} from 'react-redux';
 
 const Stack = createNativeStackNavigator();
 
