d2b78e08ff3070754dad32b9bfd8bfd290926154
AliAksoyy <alifeyza4017@gmail.com>
2024-02-22 13:36:36 +0300

    added

--- src/api/getAxiosInstance.tsx
+++ src/api/getAxiosInstance.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
-
+import {API_URI} from '@env';
 const getAxiosInstance = () => {
   const axiosInstance = axios.create({
-    baseURL: 'http://192.168.1.45:5000',
+    baseURL: API_URI,
   });
   return axiosInstance;
 };
--- src/features/authSlice/asynActions.tsx
+++ src/features/authSlice/asynActions.tsx
@@ -1,7 +1,6 @@
 import {AxiosResponse} from 'axios';
 import {AsyncThunk, createAsyncThunk} from '@reduxjs/toolkit';
 import getAxiosInstance from '../../api/getAxiosInstance';
-import {API_URI, apiKey} from '@env';
 
 const axiosInstance = getAxiosInstance();
 
@@ -14,8 +13,6 @@ interface RegisterActionParams {
 
 const registerAction: AsyncThunk<any, RegisterActionParams, {}> =
   createAsyncThunk('registerAction', async ({value}: RegisterActionParams) => {
-    console.log('ssss', API_URI);
-    console.log('ssss', apiKey);
     console.log(value);
     try {
       const res: AxiosResponse<any> = await axiosInstance.post(
